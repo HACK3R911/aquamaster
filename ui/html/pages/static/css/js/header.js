12d8c646b54ee233f5acd8955058c3df8c827763
sparta1223 <sparta12234@mail.ru>
2024-12-15 20:19:35 +0300

    Добавление товаров + header и модальные окна

--- ui/html/pages/static/css/js/header.js
+++ ui/html/pages/static/css/js/header.js
@@ -4,9 +4,7 @@ template.innerHTML = `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aquamaster</title>
     <link rel="stylesheet" href="static/css/main_style.css">
-    <script defer src="plumbing.js"></script>
     <link rel="website icon" type="png" href="../img/images/icons/search.png">
 `;
 
-// Добавляем содержимое в head
 document.head.appendChild(template.content);
